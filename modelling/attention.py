8b582683f9cf673eea9bcef8fe29c38da4c4d19b
Till <tsm.walziege@live.de>
2023-10-18 23:28:50 +0200

    Feat: Update Attention with pad and future mask

--- modelling/attention.py
+++ modelling/attention.py
@@ -3,34 +3,33 @@ import torch
 import torch.nn as nn
 
 class Attention(nn.Module):
-    def __init__(self, d_model, num_heads=1) -> None:
+    def __init__(self, mask_future=False) -> None:
         super().__init__()
-        self.d_model = d_model
-        self.num_heads = num_heads
+        self.mask_future = mask_future
 
-        self.q_linear = nn.Linear(d_model, d_model)
-        self.k_linear = nn.Linear(d_model, d_model)
-        self.v_linear = nn.Linear(d_model, d_model)
-        self.output_linear = nn.Linear(d_model, d_model)
+        #self.output_linear = nn.Linear(d_model, d_model)
 
-    def _attention(self, q, k, v):        
-        return nn.functional.softmax((q @ k.transpose(2,3) / np.sqrt(self.d_model)), dim=2) @ v
+    def _attention(self, q, k, v, mask):        
+        return nn.functional.softmax((q @ k.transpose(1,2) / np.sqrt(q.shape[-1]) + mask), dim=2) @ v
     
-    def _linear_projection(self, x: torch.Tensor, B: int):
-        q = self.q_linear(x).view(B, -1, self.num_heads, int(self.d_model/self.num_heads)).transpose(1,2)
-        k = self.k_linear(x).view(B, -1, self.num_heads, int(self.d_model/self.num_heads)).transpose(1,2)
-        v = self.v_linear(x).view(B, -1, self.num_heads, int(self.d_model/self.num_heads)).transpose(1,2)
-        return q, k, v
+    #def _linear_output(self, attn: torch.Tensor, B: int):
+    #    out = attn.transpose(1,2).contiguous().view(B, -1, self.d_model)
+    #    return self.output_linear(out)
     
-    def _linear_output(self, attn: torch.Tensor, B: int):
-        out = attn.transpose(1,2).contiguous().view(B, -1, self.d_model)
-        return self.output_linear(out)
-    
-    def forward(self, x: torch.Tensor):
-        B, N, C = x.shape
-        q, k, v = self._linear_projection(x, B)
-        attn = self._attention(q, k, v)
-        return self._linear_output(attn, B)
+    def forward(self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor, mask: torch.Tensor=None):
+        B_Q, N_Q, C_Q = query.shape
+        B_K, N_K, C_K = key.shape
+
+        future_mask = torch.zeros((N_Q, N_K))
+        
+        if self.mask_future:
+            future_mask = torch.full((N_Q, N_K), -np.Infinity)
+            future_mask = torch.triu(future_mask, diagonal=1).unsqueeze(0)
+
+        combined_mask = future_mask.masked_fill(~mask.unsqueeze(1).to(torch.bool), -np.Infinity)
+
+        # attn = self._attention(query, key, value)
+        return self._attention(query, key, value, combined_mask)
     
 
 class MaskAttention(nn.Module):
